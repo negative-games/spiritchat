dd214ec50b5f21f4ded968c0350fd3cc66df2a79
joehosten <joe@joehosten.me>
2026-07-10 16:23:30 +0100

    test: cover config reload from disk

--- platform-paper/src/test/java/gg/moonrise/chat/config/ConfigServiceTest.java
+++ platform-paper/src/test/java/gg/moonrise/chat/config/ConfigServiceTest.java
@@ -16,6 +16,34 @@ class ConfigServiceTest {
     private Path tempDir;
 
     @Test
+    void reloadPublishesConfigAndMessagesFromDisk() throws Exception {
+        ConfigService service = new ConfigService(tempDir.toFile());
+        service.init();
+
+        Files.writeString(tempDir.resolve("config.yml"), """
+                chat:
+                  static-chat-settings:
+                    enabled: true
+                    format: "<red>[Reloaded]</red> <player><dark_gray>:</dark_gray> <message>"
+                  group-chat-settings:
+                    enabled: false
+                """);
+        Files.writeString(tempDir.resolve("messages.yml"), """
+                prefix: "<aqua>Test</aqua> <dark_gray>»</dark_gray> "
+                general:
+                  reload-success: "<prefix><green>Reloaded from disk."
+                """);
+
+        service.reload();
+
+        assertEquals(
+                "<red>[Reloaded]</red> <player><dark_gray>:</dark_gray> <message>",
+                service.get().getStaticChatSettings().effectiveFormat()
+        );
+        assertEquals("<prefix><green>Reloaded from disk.", service.messages().getGeneral().getReloadSuccess().content());
+    }
+
+    @Test
     void failedReloadKeepsLastPublishedConfigAndMessages() throws Exception {
         ConfigService service = new ConfigService(tempDir.toFile());
         service.init();
